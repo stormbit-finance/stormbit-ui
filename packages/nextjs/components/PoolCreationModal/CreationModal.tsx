df2e0b47b07f5b52446786b41630f4284274e33e
Gabriela del Pilar Rojas <gabrieladelpilarrojas21@gmail.com>
2024-01-24 00:30:31 -0500

    add creationModal component

--- packages/nextjs/components/PoolCreationModal/CreationModal.tsx
+++ packages/nextjs/components/PoolCreationModal/CreationModal.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Image from "next/image";
 import Button from "../Button/Button";
 import "./CreationModal.css";
 
@@ -7,19 +9,91 @@ interface Modalprops {
 }
 
 function CreationModal(props: Modalprops) {
+  const [showInfo, setShowInfo] = useState(false);
   const { setIsModalOpen, handleSubmit } = props;
   return (
     <div className="container-modal">
-      <div className="content-modal">
-        <div className="flex gap-8">
-          <button
-            className="w-[258px] rounded-[19px] button-cancel"
-            onClick={() => {
-              setIsModalOpen();
-            }}
-          >
-            X
-          </button>
+      <div className="gap-4 content-modal">
+        <div className="flex flex-col gap-10">
+          <div className="flex justify-between">
+            <span className="text-2xl font-bold text-[#4A5056]">Create Pool</span>
+            <button
+              onClick={() => {
+                setIsModalOpen();
+              }}
+            >
+              X
+            </button>
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="text-base text-[#4A5056]">Name</span>
+            <input type="text" className="border border-solid border-[#4A5056]"></input>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span>Agreement</span>
+          <div className="flex gap-4">
+            <input type="checkbox"></input>
+            <div className="flex items-center gap-2">
+              <span>Off chain strategy</span>
+              <div
+                className="info-icon-container"
+                onMouseEnter={() => setShowInfo(true)}
+                onMouseLeave={() => setShowInfo(false)}
+              >
+                <Image src="/information.png" alt="information" width={18} height={18}></Image>
+                {showInfo && (
+                  <div className="text-xs info-tooltip text-[#484848]">
+                    <span className="text-xs font-bold">Strategy Name</span>
+                    <br></br>
+                    <span>Some description info here...</span>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <input type="checkbox"></input>
+            <div className="flex items-center gap-2">
+              <span>Providing collateral</span>
+              <div
+                className="info-icon-container"
+                onMouseEnter={() => setShowInfo(true)}
+                onMouseLeave={() => setShowInfo(false)}
+              >
+                <Image src="/information.png" alt="information" width={18} height={18}></Image>
+                {showInfo && (
+                  <div className="text-xs info-tooltip text-[#484848]">
+                    <span className="text-xs font-bold">Strategy Name</span>
+                    <br></br>
+                    <span>Some description info here...</span>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <input type="checkbox"></input>
+            <div className="flex items-center gap-2">
+              <span>Custom strategy</span>
+              <div
+                className="info-icon-container"
+                onMouseEnter={() => setShowInfo(true)}
+                onMouseLeave={() => setShowInfo(false)}
+              >
+                <Image src="/information.png" alt="information" width={18} height={18}></Image>
+                {showInfo && (
+                  <div className="text-xs info-tooltip text-[#484848]">
+                    <span className="text-xs font-bold">Strategy Name</span>
+                    <br></br>
+                    <span>Some description info here...</span>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
           <Button onClick={handleSubmit}> Submit</Button>
         </div>
       </div>
