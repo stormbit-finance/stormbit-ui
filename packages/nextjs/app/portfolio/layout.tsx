aac92a3484334adf059fd373d2d0758b47c87ed6
Gabriela del Pilar Rojas <gabrieladelpilarrojas21@gmail.com>
2024-02-18 18:33:45 -0500

    fix error in the route

--- packages/nextjs/app/portfolio/layout.tsx
+++ packages/nextjs/app/portfolio/layout.tsx
@@ -1,18 +1,23 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 function Layout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  const [activeButton, setActiveButton] = useState("My loans");
+  const [activeButton, setActiveButton] = useState("");
 
-  const handleButtonClick = (buttonText: string, route: string) => {
-    setActiveButton(buttonText);
+  useEffect(() => {
+    // Extraer la última parte de la ruta como el botón activo inicial
+    const currentRoute = window.location.pathname.split("/").pop();
+    setActiveButton(currentRoute || "myloans");
+  }, []);
+
+  const handleButtonClick = (route: string) => {
+    setActiveButton(route);
     router.push(`/portfolio/${route}`);
   };
-
   return (
     <div>
       <div className="flex flex-col gap-6 rounded-[5px] w-[1800px] h-[700px] bg-white p-10">
@@ -28,25 +33,25 @@ function Layout({ children }: { children: React.ReactNode }) {
           <div className="flex flex-col gap-3">
             <button
               className={`w-[153px] h-[45px] text-[#17344F] font-bold ${
-                activeButton === "My loans" ? "bg-[#EDEDFF] border-l-4 border-[#17344F]" : ""
+                activeButton === "myloans" ? "bg-[#EDEDFF] border-l-4 border-[#17344F]" : ""
               }`}
-              onClick={() => handleButtonClick("My loans", "myloans")}
+              onClick={() => handleButtonClick("myloans")}
             >
               My loans
             </button>
             <button
               className={`w-[153px] h-[45px] text-[#17344F] font-bold ${
-                activeButton === "My pools" ? "bg-[#EDEDFF] border-l-4 border-[#17344F]" : ""
+                activeButton === "mypools" ? "bg-[#EDEDFF] border-l-4 border-[#17344F]" : ""
               }`}
-              onClick={() => handleButtonClick("My pools", "mypools")}
+              onClick={() => handleButtonClick("mypools")}
             >
               My pools
             </button>
             <button
               className={`w-[153px] h-[45px] text-[#17344F] font-bold ${
-                activeButton === "Profile" ? "bg-[#EDEDFF] border-l-4 border-[#17344F]" : ""
+                activeButton === "profile" ? "bg-[#EDEDFF] border-l-4 border-[#17344F]" : ""
               }`}
-              onClick={() => handleButtonClick("Profile", "profile")}
+              onClick={() => handleButtonClick("profile")}
             >
               Profile
             </button>
