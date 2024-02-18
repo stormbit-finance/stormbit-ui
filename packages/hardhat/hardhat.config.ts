d7ac8dd5ca37fd1d4e73c47f9f5d0835300ca5e6
CarlosR <carlos.ramos@grupoimportia.com>
2024-02-19 01:05:18 +0700

    feat : read my pools in portfolio

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -16,6 +16,11 @@ const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr2
 // If not set, it uses the hardhat account 0 private key.
 const deployerPrivateKey =
   process.env.DEPLOYER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+const lenderPrivateKey =
+  process.env.LENDER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const borrowerPrivateKey =
+  process.env.BORROWER_PRIVATE_KEY ?? "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 // If not set, it uses ours Etherscan default API key.
 const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
 
@@ -44,6 +49,12 @@ const config: HardhatUserConfig = {
     deployer: {
       // By default, it will take the first Hardhat account as the deployer
       default: 0,
+    },
+    lender: {
+      default: 1,
+    },
+    borrower: {
+      default: 2,
     },
   },
   networks: {
@@ -133,7 +144,7 @@ const config: HardhatUserConfig = {
     },
     avalancheFuji: {
       url: "https://avalanche-fuji-c-chain.publicnode.com",
-      accounts: [deployerPrivateKey],
+      accounts: [deployerPrivateKey, lenderPrivateKey, borrowerPrivateKey],
     },
   },
   // configuration for harhdat-verify plugin
--- packages/nextjs/app/portfolio/mypools/page.tsx
+++ packages/nextjs/app/portfolio/mypools/page.tsx
@@ -1,11 +1,92 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { formatUnits } from "viem";
+import { Address, useAccount, useContractReads } from "wagmi";
 import Cheap from "~~/components/Cheap/Cheap";
 import { data } from "~~/data/data";
+import { useScaffoldContract, useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 
 function MyPools() {
   const [showCheap, setShowCheap] = useState(false);
+  const [poolList, setPoolList] = useState([] as any[]);
+
+  const account = useAccount();
+  const { data: poolAddresses, isLoading: poolAddressesLoading } = useScaffoldContractRead({
+    contractName: "StormBitCore",
+    functionName: "getPools",
+    watch: true,
+  });
+
+  const { data: LendingContract } = useScaffoldContract({
+    contractName: "StormBitLending",
+  });
+
+  const { data: pools, isLoading: poolsLoading } = useContractReads({
+    contracts:
+      LendingContract && poolAddresses && account
+        ? [
+            ...poolAddresses.map(pool => {
+              return {
+                address: pool,
+                abi: LendingContract.abi,
+                functionName: "getPoolData",
+              };
+            }),
+            ...poolAddresses.map(pool => {
+              return {
+                address: pool,
+                abi: LendingContract.abi,
+                functionName: "getVotingPower",
+                args: [account.address as Address],
+              };
+            }),
+          ]
+        : [],
+  });
+  useEffect(() => {
+    if (pools && pools.length > 0 && poolAddresses) {
+      const poolLen = pools.length / 2;
+      const poolsWithoutVotingPowerWithAddress = pools.slice(0, poolLen).map((pool, index) => {
+        const poolAddr = poolAddresses?.[index];
+        return {
+          address: poolAddr || "",
+          result: pool.result,
+        };
+      });
+      const poolsWithVotingPower = pools.slice(poolLen);
+
+      const poolsWithVotingPowerAndAddress = poolsWithoutVotingPowerWithAddress.map((pool, index) => {
+        const votingPower = poolsWithVotingPower[index].result;
+        return {
+          votingPower,
+          ...pool,
+        };
+      });
+      const filteredPools = poolsWithVotingPowerAndAddress.filter(pool => {
+        return (pool.votingPower as bigint) > BigInt(0);
+      });
+
+      setPoolList(
+        filteredPools.map(pool => {
+          const poolAddr = pool.address;
+          const availableLiquidity: bigint = pool.result.totalSupplied - pool.result.totalBorrowed;
+          const depositValue: bigint = (pool.votingPower * BigInt(pool.result.totalSupplied)) / BigInt(100);
+          return {
+            address: poolAddr || "",
+            name: pool.result ? pool.result.name : "",
+            availableLiquidity: availableLiquidity ? formatUnits(availableLiquidity, 18) : 0,
+            votingPower: pool.votingPower,
+            depositValue: depositValue ? formatUnits(depositValue, 18) : 0,
+            // borrowedAPY: "0%",
+            // suppliedAPY: "0%",
+            // totalBorrowed: pool.result ? formatUnits(pool.result.totalBorrowed, 18) : "0",
+            // totalSupplied: pool.result ? formatUnits(pool.result.totalSupplied, 18) : "0",
+          };
+        }),
+      );
+    }
+  }, [pools]);
 
   const handleDetailsClick = () => {
     setShowCheap(true);
@@ -26,14 +107,14 @@ function MyPools() {
           <span className="w-[160px] text-center">Voting Power</span>
           <span className="w-[160px] text-center"></span>
         </div>
-        {data.map(element => (
-          <div key={element.pool} className="flex gap-4 h-[95px] items-center p-8 border border-solid border-[#EAEBEF]">
-            <p className="w-[160px] text-center">{element.pool}</p>
+        {poolList.map(pool => (
+          <div key={pool.name} className="flex gap-4 h-[95px] items-center p-8 border border-solid border-[#EAEBEF]">
+            <p className="w-[160px] text-center">{pool.name}</p>
             <div className="w-[160px] text-center flex gap-1 items-center justify-center">
-              <span className="">10.01K</span>
+              <span className="">{pool.availableLiquidity}</span>
             </div>
-            <p className="w-[160px] text-center">0.00</p>
-            <p className="w-[160px] text-center">10%</p>
+            <p className="w-[160px] text-center">{pool.depositValue}</p>
+            <p className="w-[160px] text-center">{formatUnits(pool.votingPower, 0)} %</p>
             <p className="w-[160px] text-center"></p>
             <button
               onClick={handleDetailsClick}
