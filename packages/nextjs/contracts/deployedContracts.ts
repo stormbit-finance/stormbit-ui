c4e5a7a45275689d919b446ad00d7f408b9c6d7b
ngyixuan <ngyixuan2009@gmail.com>
2024-02-19 22:41:15 +0800

    fix: pass form value to request loan function

--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -354,7 +354,7 @@ const deployedContracts = {
       },
     },
     SimpleAgreement: {
-      address: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
+      address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
       abi: [
         {
           inputs: [],
@@ -556,7 +556,7 @@ const deployedContracts = {
       },
     },
     StormBitCore: {
-      address: "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
+      address: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
       abi: [
         {
           inputs: [
@@ -885,7 +885,7 @@ const deployedContracts = {
       },
     },
     StormBitLending: {
-      address: "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6",
+      address: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
       abi: [
         {
           inputs: [],
@@ -3201,7 +3201,7 @@ const deployedContracts = {
       },
     },
     StormBitLendingVotes: {
-      address: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
+      address: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
       abi: [
         {
           inputs: [],
