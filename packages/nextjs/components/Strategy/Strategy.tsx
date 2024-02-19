e247287f2e780f740af34fb374ab489a44977449
ngyixuan <ngyixuan2009@gmail.com>
2024-02-19 17:08:39 +0800

    feat: call request loan function in borrow tab

--- packages/nextjs/components/Strategy/Strategy.tsx
+++ packages/nextjs/components/Strategy/Strategy.tsx
@@ -4,7 +4,10 @@ import React, { useEffect, useState } from "react";
 import Borrow from "../Borrow/Borrow";
 import Supply from "../Supply/Supply";
 
-function Strategy() {
+interface StrategyProps {
+  poolAddress: string;
+}
+function Strategy({ poolAddress }: StrategyProps) {
   const [selectedView, setSelectedView] = useState<string>("supply");
 
   const handleButtonClick = (view: string) => {
@@ -16,7 +19,7 @@ function Strategy() {
       case "supply":
         return <Supply />;
       case "borrow":
-        return <Borrow />;
+        return <Borrow poolAddress={poolAddress} />;
       default:
         return null;
     }
