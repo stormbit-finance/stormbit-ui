25398411075b49164e45748be887e9b3d9897594
Karlita Vasquez perez <karlitavasquezperez@MacBook-Pro-de-Karlita.local>
2024-03-18 17:49:07 -0500

    expanding the background grid on the landing page

--- packages/nextjs/components/Background/Background.tsx
+++ packages/nextjs/components/Background/Background.tsx
@@ -1,34 +1,32 @@
-// components/Grid.tsx
 import { useEffect, useState } from "react";
 import "./background.css";
 
 interface GridProps {
   numRows: number;
-  numCols: number;
+  cellWidth: number;
 }
 
-const Grid: React.FC<GridProps> = ({ numRows, numCols }) => {
-  const [grid, setGrid] = useState<boolean[][]>([]);
+const Grid: React.FC<GridProps> = ({ numRows, cellWidth }) => {
+  const [numCols, setNumCols] = useState<number>(0);
 
   useEffect(() => {
-    const initializeGrid = () => {
-      const newGrid = Array(numRows)
-        .fill(false)
-        .map(() => Array(numCols).fill(false));
-      setGrid(newGrid);
-    };
+    const screenWidth = window.innerWidth;
+    const newNumCols = Math.floor(screenWidth / cellWidth);
+    setNumCols(newNumCols);
+  }, [cellWidth]);
 
-    initializeGrid();
-  }, [numRows, numCols]);
+  const grid = Array(numRows)
+    .fill(false)
+    .map(() => Array(numCols).fill(false));
 
   return (
-    <div className="absolute flex flex-col items-start justify-start overflow-hidden grid-container">
+    <div className="absolute flex flex-col items-start justify-start overflow-hidden grid-container w-full">
       {grid.map((row, rowIndex) => (
         <div key={rowIndex} className="flex flex-row items-start justify-start w-full h-full">
-          {row.map(colIndex => (
+          {row.map((col, colIndex) => (
             <div
               key={`${rowIndex}-${colIndex}`}
-              className={`cell block h-[72px] w-[72px] border border-solid border-[#ffffff04]`}
+              className={`cell block h-[72px] w-[${cellWidth}px] border border-solid border-[#ffffff04]`}
             ></div>
           ))}
         </div>
