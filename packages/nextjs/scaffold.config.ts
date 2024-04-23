ddcbef7d6cf2ca8bbcbe25dfdf468a1b2e45d346
jrcarlos2000 <jrcarlos2000.dev@gmail.com>
2024-04-23 15:01:56 +0800

    two networks: sepolia , etherlink

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -34,7 +34,7 @@ const etherlinkTestnet = defineChain({
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [etherlinkTestnet],
+  targetNetworks: [etherlinkTestnet, chains.sepolia],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
