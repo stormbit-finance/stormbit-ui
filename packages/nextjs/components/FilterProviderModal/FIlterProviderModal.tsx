5f4074cbca5168f09d0c8cbafd66a4ec14773a22
ngyixuan <67143652+ngyixuan@users.noreply.github.com>
2024-05-24 10:20:32 +0800

    Return Filter Provider Name after select options in reclaim (#64)

--- packages/nextjs/components/FilterProviderModal/FIlterProviderModal.tsx
+++ packages/nextjs/components/FilterProviderModal/FIlterProviderModal.tsx
@@ -4,8 +4,10 @@ import ModalContainer from "~~/components/ModalContainer/ModalContainer";
 
 interface ModalProps {
   setIsModalOpen: () => void;
+  setProvider: (provider: string) => void;
+  provider: string;
 }
-const FilterProviderModal: React.FC<ModalProps> = ({ setIsModalOpen }) => {
+const FilterProviderModal: React.FC<ModalProps> = ({ setIsModalOpen, provider, setProvider }) => {
   const handleCloseModal = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
       setIsModalOpen();
@@ -28,16 +30,66 @@ const FilterProviderModal: React.FC<ModalProps> = ({ setIsModalOpen }) => {
           </div>
         </div>
         <div className="p-4 w-[900px] space-y-4 text-xl">
-          <div className="cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg">Binance</div>
-          <div className="cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg">OKX</div>
-          <div className="cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg">Github</div>
-          <div className="cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg">LinkedIn Analytics</div>
-          <div className="cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg">X Analytics</div>
-          <div className="cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg">Custom</div>
+          <div
+            onClick={() => setProvider("Binance")}
+            className={`cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg ${
+              provider === "Binance" ? "bg-[#23233D]" : ""
+            }`}
+          >
+            Binance
+          </div>
+          <div
+            onClick={() => setProvider("OKX")}
+            className={`cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg ${
+              provider === "OKX" ? "bg-[#23233D]" : ""
+            }`}
+          >
+            OKX
+          </div>
+          <div
+            onClick={() => setProvider("Github")}
+            className={`cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg ${
+              provider === "Github" ? "bg-[#23233D]" : ""
+            }`}
+          >
+            Github
+          </div>
+          <div
+            onClick={() => setProvider("LinkedIn Analytics")}
+            className={`cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg ${
+              provider === "LinkedIn Analytics" ? "bg-[#23233D]" : ""
+            }`}
+          >
+            LinkedIn Analytics
+          </div>
+          <div
+            onClick={() => setProvider("X Analytics")}
+            className={`cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg ${
+              provider === "X Analytics" ? "bg-[#23233D]" : ""
+            }`}
+          >
+            X Analytics
+          </div>
+          <div
+            onClick={() => setProvider("Custom")}
+            className={`cursor-pointer hover:bg-[#23233D] px-6 py-4 rounded-lg ${
+              provider === "Custom" ? "bg-[#23233D]" : ""
+            }`}
+          >
+            Custom
+          </div>
         </div>
       </div>
       <div className="w-full flex items-center justify-center py-[20px]">
-        <Button size="large">Select</Button>
+        <Button
+          onClick={() => {
+            setProvider(provider);
+            setIsModalOpen();
+          }}
+          size="large"
+        >
+          Select
+        </Button>
       </div>
     </ModalContainer>
   );
