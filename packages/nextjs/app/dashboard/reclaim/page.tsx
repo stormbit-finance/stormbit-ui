bf201ce8cde56c3a14cc7f3b57ec37bda475e0b0
ngyixuan <67143652+ngyixuan@users.noreply.github.com>
2024-05-27 17:50:27 +0800

    Filter provider list after selection (#68)

--- packages/nextjs/app/dashboard/reclaim/page.tsx
+++ packages/nextjs/app/dashboard/reclaim/page.tsx
@@ -1,14 +1,68 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import Image from "next/image";
 import { CiFilter } from "react-icons/ci";
 import { GoArrowUpRight } from "react-icons/go";
 import Button from "~~/components/Button/Button";
 import FilterProviderModal from "~~/components/FilterProviderModal/FIlterProviderModal";
 
 const Reclaim: React.FC = () => {
+  const providerData = useMemo(
+    () => [
+      {
+        name: "Binance",
+        provider: "Binance",
+        desc: "KYC",
+        img: "/binance.svg",
+      },
+      {
+        name: "OKX",
+        provider: "OKX",
+        desc: "KYC",
+        img: "/okx.svg",
+      },
+      {
+        name: "Github",
+        provider: "Github",
+        desc: "Developer stats",
+        img: "/github.svg",
+      },
+      {
+        name: "LinkedIn Analytics",
+        provider: "LinkedIn Analytics",
+        desc: "Dashboard Analytics",
+        img: "/linkedin.svg",
+      },
+      {
+        name: "X Analytics",
+        provider: "X Analytics",
+        desc: "Dashboard Analytics",
+        img: "/x.svg",
+      },
+      {
+        name: "Custom",
+        provider: "Custom",
+        desc: "KYC",
+        img: "/custom.svg",
+      },
+    ],
+    [],
+  );
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [provider, setProvider] = useState("Filter");
+  const [providerList, setProviderList] = useState(providerData);
+
+  useEffect(() => {
+    if (provider === "All" || provider === "Filter") {
+      setProviderList(providerData);
+      return;
+    } else {
+      setProviderList(providerData.filter(item => item.provider === provider));
+    }
+  }, [provider, providerData]);
+
   return (
     <div className="flex justify-center items-center py-[30px]">
       <div className="max-w-[1380px] w-full  flex flex-col  items-center gap-[20px] pt-[30px] min-h-[800px]">
@@ -32,16 +86,13 @@ const Reclaim: React.FC = () => {
             <div className="px-10">Actions</div>
           </div>
         </div>
-
-        <div className="w-full space-y-8">
-          <div className="flex w-full justify-between items-center">
+        {providerList.map((item, index) => (
+          <div className="py-4 flex w-full justify-between items-center" key={index}>
             <div className="flex items-center">
-              <div className="w-16 h-16 p-2 flex items-center justify-center rounded-full bg-[#23233D]">
-                <span>OKX</span>
-              </div>
+              <Image width={50} height={50} className="" src={`${item.img}`} alt="" />
               <div className="ml-4">
-                <div className="text-xl">OKX Provider 1</div>
-                <div className="text-[#858BA2]">Description</div>
+                <div className="text-xl">{item.name}</div>
+                <div className="text-[#858BA2]">{item.desc}</div>
               </div>
             </div>
             <button className="flex gap-2 items-center px-8 py-3 bg-transparent text-white border border-[#374B6D] text-base  rounded-full transition">
@@ -51,25 +102,7 @@ const Reclaim: React.FC = () => {
               </span>
             </button>
           </div>
-
-          <div className="flex w-full justify-between items-center">
-            <div className="flex items-center">
-              <div className="w-16 h-16 p-2 flex items-center justify-center rounded-full bg-[#23233D]">
-                <span>Bi</span>
-              </div>
-              <div className="ml-4">
-                <div className="text-xl">Binance Provider 2</div>
-                <div className="text-[#858BA2]">Description</div>
-              </div>
-            </div>
-            <button className="flex gap-2 items-center px-8 py-3 bg-transparent text-white border border-[#374B6D] text-base  rounded-full transition">
-              Go Verify
-              <span>
-                <GoArrowUpRight />
-              </span>
-            </button>
-          </div>
-        </div>
+        ))}
       </div>
       {isModalOpen && (
         <FilterProviderModal
