5724e0ee36e740d67bbc75344b416e9de9e24423
ngyixuan <ngyixuan2009@gmail.com>
2024-07-02 15:17:58 +0800

    fix: add empty state of dashboard and reclaim

--- packages/nextjs/app/dashboard/layout.tsx
+++ packages/nextjs/app/dashboard/layout.tsx
@@ -13,15 +13,16 @@ function Layout({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <div className="pt-[100px]  flex justify-center ">
+    <div className="h-full pt-[100px]  flex justify-center ">
       <div className="h-full max-w-[1920px] w-full flex ">
-        <div className="flex flex-col gap-[14px] text-white pt-[30px] max-h-full max-w-[320px] w-full items-center text-2xl bg-[#2D2D2D]">
+        <div className="min-h-screen max-h-full flex flex-col gap-[14px] text-white pt-[30px]  max-w-[320px] w-full items-center text-2xl bg-[#2D2D2D]">
           <div className="flex flex-col gap-4 my-12">
-            <div className="flex gap-2 justify-center items-center">
-              <span className="border border-white rounded-full p-2 px-3 text-center text-sm"> y </span>
-              <span className="text-xl">{userData.username}</span>
-            </div>
-            <span className="text-[#AD7AF3] font-bold">{userData.currentBalance}</span>
+           {!userData.username && "Login Required"}
+            {userData.username && <div className="flex gap-2 justify-center items-center">
+              <span className="border border-white rounded-full p-2 px-3 text-center text-sm"> {userData?.username.slice(0,1)}</span>
+              <span className="text-xl">{userData?.username || "-"}</span>
+            </div>}
+            <span className="text-[#AD7AF3] font-bold">${userData?.currentBalance || 0.00}</span>
             <span className="text-[#C8C8C8] text-sm">Net Worth</span>
           </div>
           <ButtonLayout active={pathname === "/dashboard"} onClick={() => handleButtonClick("/")}>
@@ -45,7 +46,7 @@ function Layout({ children }: { children: React.ReactNode }) {
             Loans
           </ButtonLayout>
         </div>
-        <div className="flex flex-col gap-8 bg-[#252525] w-full text-white">{children}</div>
+        <div className="h-full flex flex-col gap-8 bg-[#252525] w-full text-white">{children}</div>
       </div>
     </div>
   );
--- packages/nextjs/app/dashboard/page.tsx
+++ packages/nextjs/app/dashboard/page.tsx
@@ -15,11 +15,11 @@ function Page() {
 
   console.log(username);
   return (
-    <div className="flex flex-col gap-10 py-10 px-14">
+    <div className="h-full flex flex-col gap-10 py-10 px-14">
       <div className="flex gap-12">
         <div className="w-[410px] h-[198px] bg-[#2F2F2F] border border-[#444C6A] rounded-[11px] flex flex-col gap-8 p-8">
           <span className="text-sm">Total Shares</span>
-          <span className="text-[#AE9FFD] text-2xl">{userData.totalShares}</span>
+          <span className="text-[#AE9FFD] text-2xl">${userData?.totalShares || 0}</span>
           <div>
             <Button backgroundColor="#AE9FFD">
               Deposit <AiOutlineDownload />
@@ -29,35 +29,40 @@ function Page() {
         <div className="w-[410px] h-[198px] bg-[#2F2F2F] border border-[#444C6A] rounded-[11px] p-8">
           <div className="flex flex-col gap-7">
             <span className="text-sm">Total Deposited</span>
-            <span className="text-[#AE9FFD] text-2xl">{userData.totalDeposited}</span>
+            <span className="text-[#AE9FFD] text-2xl">${userData?.totalDeposited || 0}</span>
           </div>
           <div className="mt-4">
-            <span className="text-[10px] text-[#A4A4A4]">Total Earning</span>
+            <span className="text-[14px] text-[#A4A4A4]">Total Earning</span>
             <div className="flex gap-3">
-              <span className="text-[#FAFF00] text-[10px]">{userData.totalEarnings}</span>
-              <span className="text-[#2CFF74] text-[10px]">{userData.earningsPercentage}</span>
+              <span className="text-[#FAFF00] text-[14px]">{userData?.totalEarnings || 0.00}%</span>
+              <span className="text-[#2CFF74] text-[14px]">{userData?.earningsPercentage || 0.00}%</span>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="h-full flex flex-col gap-6">
         <span className="text-xl">Transactions</span>
-        <div className="w-full min-h-screen bg-[#2F2F2F] border border-[#444C6A] rounded-[11px]">
-          {userData.transactions.map((transaction, index) => (
-            <div className="p-10 flex w-full justify-between items-center" key={index}>
+        <div className="h-full w-full p-10 gap-6 flex flex-col bg-[#2F2F2F] border border-[#444C6A] rounded-[11px]">
+        {userData.transactions.length === 0 ? (
+              <div className="flex  justify-center items-center  text-[#A8B1C8] text-center">No data here</div>
+            ):
+        
+        (  userData.transactions.map((transaction, index) => (
+            <div className=" flex w-full justify-between items-center" key={index}>
               <div className="flex items-center">
-                <Image width={30} height={30} className="" src="/icontransactions.svg" alt="transaction icon" />
+                <Image width={40} height={40} className="" src="/icontransactions.svg" alt="transaction icon" />
                 <div className="ml-4">
-                  <div className="text-sm">{transaction.type}</div>
+                  <div className="text-sm">{transaction?.type || ""}</div>
                   <div className="text-xs text-[#858BA2]">06/14/2024 15:24 pm</div>
                 </div>
               </div>
               <div className="flex gap-4 justify-center items-center">
-                <span className="text-[#AE9FFD] text-xl">{transaction.amount}</span>
+                <span className="text-[#AE9FFD] text-xl">${transaction?.amount|| 0.00}</span>
                 <span className="text-sm">USD</span>
               </div>
             </div>
-          ))}
+          )))
+        }
         </div>
       </div>
     </div>
