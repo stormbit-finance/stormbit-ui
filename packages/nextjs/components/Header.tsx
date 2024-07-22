bd2db87f450f15fb112f51a45d824d4e2cd547ab
ngyixuan <ngyixuan2009@gmail.com>
2024-07-22 16:11:06 +0800

    fix: landing mobile responsive layout optimization

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -83,18 +83,16 @@ export const Header = () => {
         <div className="flex h-[100px] justify-center absolute top-0 z-40  w-full backdrop-blur-lg transition-colors duration-500 bg-[#f8fafc0f]/5">
           <div className="flex justify-between items-center min-h-0 lg:py-2 lg:px-6 text-white lg:static navbar sm:px-2 max-w-[1920px]  xl:max-w-full w-full p-2">
             <div className="flex gap-3 items-center">
-              <Link href="/" passHref className="flex ml-4 mr-6">
-                <div>
-                  <Image src="/logo.png" alt="logo" width={50} height={50} priority className="lg:w-[80px]" />
-                </div>
-                <div className="flex justify-center items-center">
-                  <span className="text-xs bg-[#FFEB80] px-4 py-1 rounded-[7px] text-black">Beta</span>
-                </div>
+              <Link href="/" passHref className="relative ml-1 lg:ml-4 lg:mr-6">
+                <Image src="/logo.png" alt="logo" width={50} height={50} priority className="w-[70px] lg:w-[80px]" />
+                <span className="absolute left-16 top-5 lg:left-20 text-xs  lg:text-sm bg-[#FFEB80] px-4 py-1 rounded-[7px] text-black">
+                  Beta
+                </span>
               </Link>
             </div>
             <div className="flex-grow gap-8 mr-4 navbar-end">
               <button
-                className="text-black lg:py-3 lg:px-6  bg-[#D0C8FF] rounded-[5px] lg:text-xl py-1 px-2 text-sm"
+                className="text-black py-3 px-4 lg:px-6  bg-[#D0C8FF] rounded-[5px]  text-base lg:text-xl"
                 onClick={() => router.push("/lenders")}
                 type="button"
               >
